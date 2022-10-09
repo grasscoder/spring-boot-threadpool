95c8d0e70081b781b8d7462b20387ecdfaf08de8
baiyang <hu66hu@163.com>
2022-10-09 23:09:48 +0800

    使用线程池导数据

--- src/main/java/org/test/runnable/OutputTask.java
+++ src/main/java/org/test/runnable/OutputTask.java
@@ -2,7 +2,6 @@ package org.test.runnable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.test.controller.UserController;
 
 import java.io.BufferedWriter;
 import java.io.File;
