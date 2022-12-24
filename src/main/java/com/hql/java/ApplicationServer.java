09e34a5e62635e3465f6be826d06756c16ca6930
baiyang <hu600hu@163.com>
2022-12-24 22:37:02 +0800

    增加nacos配置

--- src/main/java/com/hql/java/ApplicationServer.java
+++ src/main/java/com/hql/java/ApplicationServer.java
@@ -1,20 +1,38 @@
 package com.hql.java;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.naming.NamingService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("org.test.mapper")
 @SpringBootApplication
-public class ApplicationServer {
+public class ApplicationServer implements CommandLineRunner {
     private static final Logger logger= LoggerFactory.getLogger(ApplicationServer.class);
+    @NacosInjected
+    private NamingService namingService;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${server.port}")
+    private Integer serverPort;
 
 
     public static void main(String[] args) {
         logger.info("服务开始启动");
         SpringApplication.run(ApplicationServer.class);
         logger.info("服务启动完成");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // 通过Naming服务注册实例到注册中心
+        namingService.registerInstance(applicationName, "127.0.0.1", serverPort);
     }
 }
